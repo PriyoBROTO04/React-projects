8527d2aa24afb50ac16f404994fcf02b187b800f
Priyobroto Kar <priyobrotokar@gmail.com>
2023-05-06 12:08:00 +0530

    Added Material UI icons

--- incrementdecrement/src/App.js
+++ incrementdecrement/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import { Button } from '@mui/material';
 import './App.css';
 
 function App() {
+  const [count,setCount]=useState(0);
+
+  const incCount=()=>{
+    setCount(count+1)
+  }
+  const decCount=()=>{
+    if(count===0){
+      alert("Sorry, Zero limit reached")
+    }else{
+
+      setCount(count-1)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <div className='counter'>{count}</div>
+      <div className='buttons'>
+        <Button variant='contained' className='add' onClick={incCount}><AddIcon fontSize='large'/></Button>
+        <Button variant='contained' className='remove' onClick={decCount}><RemoveIcon fontSize='large'/></Button>
+      </div>
+
     </div>
   );
 }
